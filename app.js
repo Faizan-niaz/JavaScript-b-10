5ae897344f7fedb6fd187fe9353c4d0faddee33f
Faizan-niaz <faizan22niaz@gmail.com>
2024-01-01 01:13:14 +0500

    updated app js

---
+++ app.js
@@ -0,0 +1,38 @@
+
+// var totalCost = 1 + 3 * 4;
+// console.log(totalCost)
+
+
+//Arrays
+
+var arr = [];
+arr[0] = "kamran";
+arr[1] = "Bilal";
+arr[2] = "Ali";
+arr[3] = "saqib";
+arr[4] = "ganja";
+
+// arr.pop();
+// arr.pop();
+
+// arr.push("Zafran","Dildar","Murshid");
+// arr.pop();
+
+// arr.shift();
+// arr.shift();
+// arr.unshift("Imran khan","Murad Saeed");
+arr.splice(2,2,"umer Amin","I-khan","terrorist")
+var slcVlue = arr.slice(2,5);
+console.log(arr)
+console.log(slcVlue)
+
+
+
+// var students = ["faizan","bilal","Mohsin","Umar","umer"];
+
+// var addStud = students.push("UOH");
+// students.pop();
+// console.log(students)
+
+
+
